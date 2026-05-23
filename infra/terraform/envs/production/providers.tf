3e76cca1705ffc44ecc92d2c06877d37c58cd426
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-05-23 03:51:41 -0300

    update

--- infra/terraform/envs/production/providers.tf
+++ infra/terraform/envs/production/providers.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
   default_tags {
     tags = {
-      Environment = "dev"
+      Environment = "production"
       ManagedBy   = "Terraform"
       Repository  = "report-service"
       Project     = "report-service"
